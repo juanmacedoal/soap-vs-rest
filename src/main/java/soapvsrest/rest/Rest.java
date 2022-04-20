0b31889c1e0cbcea40a0e440bed6d7e93cd57e39
= <juan-manuel.macedo@soprasteria.com>
2022-04-20 14:21:52 +0200

    add rest calls

--- src/main/java/soapvsrest/rest/Rest.java
+++ src/main/java/soapvsrest/rest/Rest.java
@@ -1,3 +1,31 @@
 package soapvsrest.rest;
 
-public interface Rest {}
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * Rest example: Represents the best practices of Restful and the example of this architecture
+ * pattern.
+ */
+public interface Rest {
+
+  /**
+   * Get example.
+   *
+   * @return String
+   */
+  @GetMapping(path = "/example", produces = APPLICATION_JSON_UTF8_VALUE)
+  ResponseEntity<String> getExample();
+
+  /**
+   * Post example.
+   *
+   * @return
+   */
+  @PostMapping(path = "/example/{example}", produces = APPLICATION_JSON_UTF8_VALUE)
+  ResponseEntity<String> postExample(@PathVariable String example);
+}
--- src/main/java/soapvsrest/rest/impl/RestImpl.java
+++ src/main/java/soapvsrest/rest/impl/RestImpl.java
@@ -1,3 +1,35 @@
 package soapvsrest.rest.impl;
 
-public class RestImpl {}
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import soapvsrest.rest.Rest;
+
+@RestController
+@RequestMapping(path = "restvssoap")
+public class RestImpl implements Rest {
+
+  /**
+   * Get example.
+   *
+   * @return String
+   */
+  @Override
+  public ResponseEntity<String> getExample() {
+    return new ResponseEntity<>("Example of a get api rest", HttpStatus.ACCEPTED);
+  }
+
+  /**
+   * Post example.
+   *
+   * @param example Example data
+   * @return Response entity example string
+   */
+  @Override
+  public ResponseEntity<String> postExample(@PathVariable String example) {
+    System.out.println("Data that came from the client: " + example);
+    return new ResponseEntity<>(example, HttpStatus.CREATED);
+  }
+}
